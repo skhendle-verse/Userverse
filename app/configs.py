461726ea8067d27da15e358da929801ee7cd6c79
Sandile <skhendle@gmail.com>
2025-04-22 20:50:00 +0200

    configs setup

--- app/configs.py
+++ app/configs.py
@@ -1,4 +1,13 @@
 import json
+from app.utils.db_config import DatabaseConfig
+
 
 with open("sample-config.json") as f:
-    configs = json.load(f)
+    data = json.load(f)
+
+
+environment = data.get("environment", "dev")
+configs = {
+    "environment": environment,
+    "database_url": DatabaseConfig.get_connection_string(data, environment),
+}
