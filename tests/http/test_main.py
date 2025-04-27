e9a17049612d6bd8c17c88293c4c4a3e735fae5c
Sandile <skhendle@gmail.com>
2025-04-28 00:58:11 +0200

    setting up HTTP testing

---
+++ tests/http/test_main.py
@@ -0,0 +1,6 @@
+from tests.http.client import client
+
+def test_read_main():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message":"Welcome to the Userverse backend API"}
---
+++ tests/utils/basic_auth.py
@@ -0,0 +1,16 @@
+import base64
+
+def get_basic_auth_header(username: str, password: str) -> dict:
+    """
+    Generate the Basic Auth header for HTTP requests.
+
+    Args:
+        username (str): The username (email).
+        password (str): The password.
+
+    Returns:
+        dict: A dictionary with the Authorization header.
+    """
+    credentials = f"{username}:{password}"
+    encoded_credentials = base64.b64encode(credentials.encode()).decode()
+    return {"Authorization": f"Basic {encoded_credentials}"}
